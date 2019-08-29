788d0d87502733f7e38c470cce06ab238506269f
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-08-29 17:34:37 +0100

    creating a number of routes

--- hello.py
+++ hello.py
@@ -3,5 +3,32 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show user profile for that user
+    return 'user %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath afterh /path/
+    return 'Subpath %s' % escape(subpath)
+
+@app.route('/projects/')
+def projects():
+    return 'THe projects page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
